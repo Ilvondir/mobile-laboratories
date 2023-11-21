257d453b16c446eb59d4981faeab168ca3eb8db4
Michał Komsa <101294147+Ilvondir@users.noreply.github.com>
2023-11-21 21:35:53 +0100

    Add files via upload

---
+++ Lab5/Lab5/pages/MusicPlayer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const MusicPlayer = () => {
+    return (
+        <View>
+            <Text>MusicPlayer</Text>
+        </View>
+    );
+};
+
+export default MusicPlayer;
---
+++ Lab5/Lab5/pages/VideoPlayer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const VideoPlayer = () => {
+    return (
+        <View>
+            <Text>VideoPlayer</Text>
+        </View>
+    );
+};
+
+export default VideoPlayer;
